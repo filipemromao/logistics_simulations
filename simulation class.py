61ef7443a9840128d90bbff34e2ceb63cd0d061f
FMR <filipemromao@gmail.com>
2021-10-23 08:02:30 -0300

    mudança acento

--- simulation class.py
+++ simulation class.py
@@ -2,7 +2,7 @@ from Material import Material
 from Distriburion_Center import Distribuition_Center
 
 itens = []
-itens.append(Material('123456', 'Cafe teste', 30000))
+itens.append(Material('123454', 'Cafe teste', 30000))
 itens.append(Material('654321', 'Cafe inicial', 50000))
 
 cd = Distribuition_Center('ne', itens, 5000, 6)
